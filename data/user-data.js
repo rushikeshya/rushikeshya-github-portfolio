252703f7682d3910a8f20e8aca9fc242df08ffe9
Rushikesh Yadav <rushikeshyadav301220@gmail.com>
2024-05-15 18:26:50 +0530

    Add files via upload

---
+++ data/user-data.js
@@ -0,0 +1,26 @@
+export const userData = {
+  githubUser: 'rushikeshya',
+  github: 'https://github.com/rushikeshya',
+  hackerrank: 'https://www.hackerrank.com/profile/rushikeshyadav31',
+  linkedIn: 'https://linkedin.com/in/rushikeshyadav149/',
+  twitter: 'https://twitter.com/rushi_yadav1122',
+  leetcode: "https://leetcode.com/u/rushikesh_yadav/",
+  resume: "https://drive.google.com/file/d/1uICZYPUWmwiRqAv7zFZ_4BCXsmaWkHyw/view?usp=sharing",
+  skills: ['Python', 'SQL', 'Machine Learning', 'Deep Learning', 'Statistics', 'MySql', 'Oracle', 'MongoDB', 'Docker', 'Recommendation Systeams']
+};
+
+
+
+
+
+
+
+/*export const userData = {
+  githubUser: 'said7388',
+  github: 'https://github.com/rushikeshya',
+  linkedIn: 'https://linkedin.com/in/rushikeshyadav149/',
+  twitter: 'https://twitter.com/rushi_yadav1122',
+  hackerrank: "https://hackerrank.com/profile/rushikeshyadav31",
+  resume: "",
+  skills: ['Python', 'SQL', 'Machine Learning', 'Deep Learning', 'Statistics', 'MySql', 'Oracle', 'MongoDB', 'Docker', 'Recommendation Systeams'],
+};*/
